8c13ba3a3996bcee8a73a8867d34932c79ffe976
Rohan Saxena <rohansaxena@192.168.1.6>
2024-12-04 22:32:19 +0530

    Solving for show info on click in mobile view

--- js/home.js
+++ js/home.js
@@ -6,3 +6,11 @@ hamburger.addEventListener('click', function() {
   this.classList.toggle('open');
   dropdown.classList.toggle('open');
 });
+
+// Toggle the contact detail on icon click
+document.querySelectorAll('.contact-info .icon-container').forEach(function(icon) {
+  icon.addEventListener('click', function() {
+    // Toggle the clicked class to show/hide the contact detail
+    icon.classList.toggle('clicked');
+  });
+});
